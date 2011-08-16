380300b64a4282a5210e420da1fc16a6c5f6b6df
Dave <david.bethke@gmail.com>
2011-08-16 14:55:13 -0500

    first commit

---
+++ Spike/src/objectFile.js
@@ -0,0 +1,72 @@
+//namespace definition
+var ObjectFile = ObjectFile || {};
+
+ObjectFile.Datastore = function(){
+	//private static
+	var nextId=1;
+	
+	return function(){
+		//private instance
+		var cacheId=nextId++;
+		var cacheSize='unassigned';
+		var cacheType='unassigned';
+		//public (this instance only)
+		this.getId= function(){ return cacheId;};
+		this.getSize= function(){ return cacheSize;};
+		this.getType= function(){return cacheType;};
+		this.setSize= function(size){cacheSize=size;};
+		this.setType= function(type){cacheType=type;};	
+	};
+	
+}();
+
+ObjectFile.Datastore.prototype.read= function(key){
+	return localStorage[key];
+	//return "Read";
+};
+ObjectFile.Datastore.prototype.readAllOF= function(){
+	var results= new Array();
+	for(var i=0; i< localStorage.length;i++){
+		results[i]=JSON.parse(localStorage[i]);
+		
+	}
+	return results;
+	//return "Read";
+};
+ObjectFile.Datastore.prototype.write= function(key,value){
+	
+	if( window.localStorage == 'undefined'){
+		return 'no localstorage defined';
+	}
+	else{
+	
+	localStorage[key]=value;
+	return localStorage[key];
+	}
+};
+ObjectFile.Datastore.prototype.writeOF= function(key,value){
+	
+	if( window.localStorage == 'undefined'){
+		return 'no localstorage defined';
+	}
+	else{
+	
+	localStorage[key]=JSON.stringify(value);
+	//return localStorage[key];
+	}
+};
+ObjectFile.Datastore.prototype.remove = function(){
+	return "Remove";		
+};
+ObjectFile.Datastore.prototype.flush = function(){
+	localStorage.clear();
+	return "Flush";
+};
+ObjectFile.Datastore.prototype.update = function(){
+	return "Update";
+};
+ObjectFile.Datastore.prototype.length = function(){
+	return localStorage.length;
+};/**
+ * 
+ */
---
+++ Spike/src/weapon.js
@@ -0,0 +1,107 @@
+//listener to click on form
+//init to fill in select
+
+//fill out the select in the page
+window.addEventListener('load', eventWindowLoaded,false);
+
+function eventWindowLoaded(){
+	initForm();
+	document.getElementById('formWeapon').onmouseup=weaponController;
+	document.getElementById('formAmmo').onmouseup=ammoController;
+
+}
+
+function initForm(){
+	//var weapons=['rifles','pistol','revolver'];
+	
+	
+	//init dao Manager, get weaponDAO, ammoDAO
+	daoManager= new DAOManager();
+	var weaponDAO=daoManager.getWeaponDAO();
+	var weapons=weaponDAO.getWeaponList();
+	
+	var ammoDAO=daoManager.getAmmoDAO();
+	var ammo=ammoDAO.getAmmoList();
+	
+	var selectWeapon=document.getElementById('selectWeapon');
+	var selectAmmo=document.getElementById('selectAmmo');
+	//reset options length(remove 'Selected' text
+	selectWeapon.options.length=0;
+	selectAmmo.options.length=0;
+	
+	//get values to fill in
+	for (var i=0; i< weapons.length; i++){
+		//document.write('Updating:'+weapons[i]);
+		selectWeapon.options[i]=new Option(weapons[i]['name'],i);
+		//selectWeapon.options[i].Text=weapons[i];
+	}
+	for(var i=0; i< ammo.length;i++){
+		selectAmmo.options[i]=new Option(ammo[i],i);
+	}
+	
+}
+
+function weaponController(){
+	var selected=document.getElementById('selectWeapon').selectedIndex;
+	var options=document.getElementById('selectWeapon').options;
+	alert('You clicked:'+options[selected].text);
+}
+function ammoController(){
+	var selected=document.getElementById('selectAmmo').selectedIndex;
+	var options=document.getElementById('selectAmmo').options;
+	alert('You clicked:'+options[selected].text);
+}
+
+function DAOManager(){
+	var weaponDAO=new WeaponDAO();
+	var ammoDAO= new AmmoDAO();
+	this.getWeaponDAO=function(){return weaponDAO;};
+	this.getAmmoDAO=function(){return ammoDAO;};
+}
+function WeaponDAO(){
+	var weaponList=new Array();
+	//var weaponOF= new WeaponOF();
+	//weaponList= weaponOF.readAll();
+	//init weapon object file
+	var OFweapon= new ObjectFile.Datastore();
+	OFweapon.setType('weapon');
+	OFweapon.setSize(16);
+	//init w/ some vals
+	initWeaponOF(OFweapon);
+	//get list of results
+	weaponList=OFweapon.readAllOF();
+	
+	this.getWeaponList=function(){ return weaponList;};
+}
+function AmmoDAO(){
+	var ammoList;
+	var ammoOF= new AmmoOF();
+	ammoList=ammoOF.readAll();
+	this.getAmmoList=function(){return ammoList;};
+}
+function WeaponOF(){
+	
+	var weaponList=['rifle','pistol','revolver'];
+	this.readAll=function(){
+		return weaponList;
+	};
+}
+function AmmoOF(){
+	var ammoList=['9mm','38spl','357mag'];
+	this.readAll=function(){
+		return ammoList;
+	};
+}
+function initWeaponOF(objectFile){
+	objectFile.flush();
+	//var weapons=['rifle','pistol','revolver','glock'];
+	var weapons=[
+	             {name:'rifle',number:'0'},
+	             {name:'pistol',number:'1'},
+	             {name:'revolver',number:'2'},
+	             {name:'glock',number:'3'}];
+	for(var i=0; i< weapons.length; i++){
+		objectFile.writeOF(i,weapons[i]);
+	}
+}
+
