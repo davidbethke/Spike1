2cd8e2fc1ecc6b38a648753913cd9786357a28b4
Dave <david.bethke@gmail.com>
2011-08-18 17:56:36 -0500

    added drills object, objectfile, trying out adding div need to add show/hide of div, and determine which <p> was selected

--- Spike/src/weapon.js
+++ Spike/src/weapon.js
@@ -1,6 +1,6 @@
 //model view controller for Object File access
 window.addEventListener('load', eventWindowLoaded,false);
-
+var type={Weapon:'0',Ammo:'1',Target:'2',Drills:'3'};
 function eventWindowLoaded(){
 	localStorage.clear();
 	var daoManager= new DAOManager();
@@ -9,11 +9,23 @@ function eventWindowLoaded(){
 	document.getElementById('formWeapon').addEventListener('mouseup',function(){genericController(daoManager,0)},false);
 	document.getElementById('formAmmo').addEventListener('mouseup',function(){genericController(daoManager,1)},false);
 	document.getElementById('formTarget').addEventListener('mouseup',function(){genericController(daoManager,2)},false);
+	document.getElementById('divDrills').addEventListener('mouseup',function(){genericControllerDiv(daoManager,3)},false);
+
 }
 
 
 function initForm(daoManager){
 	//fill out selection options in form
+	//generic might be something like this
+	//var daoList= new Array();
+	//var genericFill=new Array();
+	//
+	//for(var i=0; i<numberOfObjects;i++){
+	//	daoList[i]=daoManager.getDAOList[i];
+	//  var genericFill[i]= daoList[i].getList();
+	//  }
+	// later replace fill with genericFill
+	
 	var weaponDAO=daoManager.getWeaponDAO();
 	var weapons=weaponDAO.getList();
 	
@@ -22,10 +34,17 @@ function initForm(daoManager){
 
 	var targetDAO=daoManager.getTargetDAO();
 	var targets=targetDAO.getList();
+	
+	// hack for div
+	var drillsDAO=daoManager.getDrillsDAO();
+	var drills=drillsDAO.getList();
 
 	var selectWeapon=document.getElementById('selectWeapon');
 	var selectAmmo=document.getElementById('selectAmmo');
 	var selectTarget=document.getElementById('selectTarget');
+	
+	//hack for div
+	var selectDrills=document.getElementById('divDrills');
 
 	//reset options length(remove 'Selected' text
 	selectWeapon.options.length=0;
@@ -36,19 +55,50 @@ function initForm(daoManager){
 	var fill=[weapons,ammo,targets];
 	fillSelect(select,fill);
 	
+	//hack for div, call fillDiv
+	var drillSelect=[selectDrills];
+	var drillFill=[drills];
+	fillDiv(drillSelect,drillFill);
+	
 	
 }
 
 //generic to fix above duplicate code maybe, still needs work
 function fillSelect(select,fill){
 	//fill for each element in array
-	for(k=0; k<select.length;k++){
+	for(var k=0; k<select.length;k++){
 		for(var i=0; i< fill[k].length;i++){
 			select[k].options[i]=new Option(fill[k][i].name,i);
 		}
 	}
 }
 
+// for div show/hide functionality instead of select list
+function fillDiv(select,fill){
+for(var k=0; k<select.length;k++){
+	for(var i=0; i<fill[k].length;i++){
+		drawP(select[k],fill[k][i],i);
+	}
+ }
+}
+
+// function to create a node like <p id='drills0' value='0' class='drills' checked='false'>fill.name</p>
+function drawP(select,fill,value){
+	//var divSelect = 'div'+select;
+	//var selectElement=document.getElementById(divSelect); // get handle of div element to append to
+	var newElement=document.createElement('p');
+	var newText=document.createTextNode(fill.name);
+	newElement.setAttribute('id',select.id+value);
+	newElement.setAttribute('value',value);
+	newElement.setAttribute('class',select.id);
+	newElement.setAttribute('checked','false');
+	newElement.appendChild(newText);
+	//selectElement.appendChild(newElement);
+	select.appendChild(newElement);
+	//document.getElementById(select.id+value).innerHtml=fill.name;
+	}
+
+//genericController assumes select statement, not a DIV (requires genericControllerDiv
 function genericController(daoManager,number){
 	var genericDAO=daoManager.getDAOList(number);
 	var type=genericDAO.getType();
@@ -58,6 +108,15 @@ function genericController(daoManager,number){
 	var generic= genericDAO.getItem(options[selected].value);
 	var propString=getProps(generic);
 	alert ('You Clicked:'+options[selected].text+propString);
+}
+
+function genericControllerDiv(daoManager,number){
+	var genericDAO=daoManager.getDAOList(number);
+	var type= genericDAO.getType();
+	type='div'+type;
+	// need to figure out how to parse thru list of class=type and look at the value of 'checked' as true, must add a listener to each <p>
+	// for now alert
+	alert('You clicked:'+type);
 }
 
 function getProps(item){
@@ -89,8 +148,13 @@ function DAOManager(){
 	             new Target('target2','today'),
 	             new Target('target3','today'),
 	             new Target('target4','yesterday')];
+	var drills=[
+	            	new Drills('drill1','standing triangle','two hand','low ready','1-1-1'),
+	            	new Drills('drill2','standing triangle','two hand','low ready','1-2-3'),
+	            	new Drills('drill3','standing off shoulder','two hand','low ready','1-1-1'),
+	            	new Drills('drill4','standing off shoulder','two hand','low ready','1-2-3')];
 	
-	var initObjs=[weapons,ammo,targets];
+	var initObjs=[weapons,ammo,targets,drills];
 	var objectFiles=new Array();
 	objectFiles=this.getObjectFiles();
 	
@@ -107,6 +171,7 @@ function DAOManager(){
 	this.getWeaponDAO=function(){return daoList[0];};
 	this.getAmmoDAO=function(){return daoList[1];};
 	this.getTargetDAO=function(){return daoList[2];};
+	this.getDrillsDAO=function(){return daoList[3];};
 	//to support generic controller
 	this.getDAOList=function(number){return daoList[number];};
 };
@@ -116,7 +181,8 @@ DAOManager.prototype.getObjectFiles=function(){
 	var	nameSize=[
 	   	          {name:'Weapon',size:'16'},
 	   	          {name:'Ammo',size:'16'},
-	   	          {name:'Target',size:'16'}
+	   	          {name:'Target',size:'16'},
+	   	          {name:'Drills',size:'16'}
 	   	          ];
 	for(var i=0; i< nameSize.length;i++){
 		objectFiles[i]= new ObjectFile.Datastore();
@@ -167,5 +233,13 @@ function Ammo(name,vendor,bullet){
 function Target(name,date){
 	this.name=name;
 	this.date=date;
+}
+//try for shooting drills, call it drills
+function Drills(name,stance,grip, draw,firePattern){
+	this.name=name;
+	this.stance=stance;
+	this.grip=grip;
+	this.draw=draw;
+	this.firePattern=firePattern;
 }
 
